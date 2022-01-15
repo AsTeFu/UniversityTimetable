015daebb558efacb4461699a5afecca7842fb3eb
Vyacheslav Khvostenko <vyacheslav.khvostenko@7bits.it>
2022-01-15 11:06:04 +0600

    [+] added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: false,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+};
